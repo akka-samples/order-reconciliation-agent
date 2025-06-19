432fbb43d748b02825c5336a2531dc24ba055858
Alan <aklikic@gmail.com>
2025-06-19 11:44:51 +0200

    bump 3.3.2 removing CompletionStage due to virtual thread support replacing SimpleHttpClient with HttpClient (using HttpClientProvider for ai-agentic-service discovery)

--- ai-agentic-service/src/main/java/com/akka/api/PromptEndpoint.java
+++ ai-agentic-service/src/main/java/com/akka/api/PromptEndpoint.java
@@ -7,8 +7,6 @@ import akka.javasdk.annotations.http.Post;
 import akka.javasdk.client.ComponentClient;
 import com.akka.application.PromptEntity;
 
-import java.util.concurrent.CompletionStage;
-
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
 @HttpEndpoint("/api/prompts")
 public class PromptEndpoint {
@@ -20,17 +18,17 @@ public class PromptEndpoint {
     }
 
     @Get("/{promptName}")
-    public CompletionStage<PromptEntity.PromptTemplate> getPromptByName(String promptName) {
+    public PromptEntity.PromptTemplate getPromptByName(String promptName) {
         return componentClient.forEventSourcedEntity(promptName)
                 .method(PromptEntity::getPromptTemplate)
-                .invokeAsync();
+                .invoke();
     }
 
     @Post()
-    public CompletionStage<PromptEntity.PromptTemplate> addPrompt(PromptEntity.PromptTemplate request) {
+    public PromptEntity.PromptTemplate addPrompt(PromptEntity.PromptTemplate request) {
         return componentClient
                 .forEventSourcedEntity(request.name())
                 .method(PromptEntity::addPromptTemplate)
-                .invokeAsync(request);
+                .invoke(request);
     }
 }
